06667fa3b412ce7b96ce3a758502ab8b0b3f61f6
goutham varma <goutham@bluvision.com>
2021-04-09 11:49:54 +0530

    added broadcast receiver for airplane mode

--- app/src/main/java/com/example/lmsandroid/LoginActivity.kt
+++ app/src/main/java/com/example/lmsandroid/LoginActivity.kt
@@ -1,5 +1,9 @@
 package com.example.lmsandroid
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
@@ -29,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
 
         buttonLogin.setOnClickListener {
 
-            val     email = editTxtEmail.text.toString().trim()
+            val email = editTxtEmail.text.toString().trim()
             val password = editTxtPassword.text.toString().trim()
 
 
@@ -57,6 +61,44 @@ class LoginActivity : AppCompatActivity() {
         }
 
         }
+
+
+    override fun onStart() {
+        super.onStart()
+        val intentFilter = IntentFilter("android.intent.action.AIRPLANE_MODE")
+        registerReceiver(wifiStateReceiver, intentFilter)
+    }
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(wifiStateReceiver)
+    }
+    private val wifiStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getIntExtra(
+                WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN
+            )) {
+                WifiManager.WIFI_STATE_ENABLED -> {
+
+                    Toast.makeText(this@LoginActivity, "Wifi is On", Toast.LENGTH_SHORT).show()
+                }
+                WifiManager.WIFI_STATE_DISABLED -> {
+
+                    Toast.makeText(this@LoginActivity, "Wifi is Off", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            val isAirplaneModeOn = intent.getBooleanExtra("state", false);
+            if(isAirplaneModeOn){
+
+                // handle Airplane Mode on
+                Toast.makeText(this@LoginActivity, "Airplane Mode is On", Toast.LENGTH_SHORT).show()
+            } else {
+                // handle Airplane Mode off
+                Toast.makeText(this@LoginActivity, "Airplane Mode Mode is Off", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 
     fun validateEmail(): Boolean {
